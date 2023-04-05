11da4af89b57e4ee08842914b60e465aaa480e3e
AnitaKivindyo <anitakivindyo@gmail.com>
2023-04-05 10:49:52 +0300

    Updated python If-else

--- python If-Else.py
+++ python If-Else.py
@@ -1,13 +1,14 @@
-if __name__ == '__main__':
-    n= int(input().strip())
+import math
 
-        
+if __name__ == '__main__':
+    n= int(input().strip()) 
+
     if n%2 != 0:
         print ('Weird')
     else:
-        if 2<= n >= 5:
+        if 2<= n <= 5:
             print ('Not Weird')
-        elif 6<= n >= 20:
+        elif 6<= n <= 20:
             print('Weird')
         else:
             print('Not Weird')
